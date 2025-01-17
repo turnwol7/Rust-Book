1b4af7aa9300621eb42338dca368fabfae6560f5
turnwol7 <justinb.developer@gmail.com>
2025-01-17 14:12:39 -0400

    finished slices

--- slice-type/src/main.rs
+++ slice-type/src/main.rs
@@ -1,28 +1,36 @@
 fn main() {
-let mut s = String::from("hello world");
+    let my_string = String::from("hello world");
 
-    let word = first_word(&s); // word will get the value 5
+    //&str is literal reference, immutable. String is heap dynamic sizing
 
-    s.clear(); // this empties the String, making it equal to ""
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word(&my_string);
 
-    // word still has the value 5 here, but there's no more string that
-    // we could meaningfully use the value 5 with. word is now totally invalid!}
+    let my_string_literal = "hello world";
 
-    let s = String::from("hello world");
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
 
-    let hello = &s[0..5];
-    let world = &s[6..11];
-
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
 }
 // example of the long way around. we look for the first space and return 5 for hello
-fn first_word(s: &String) -> usize {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
     
-    s.len()
+    &s[..]
+    
+
 }
