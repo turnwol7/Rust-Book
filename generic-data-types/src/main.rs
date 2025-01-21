7fd5bc1ed82144816cb9afca66547ac356a42fc0
turnwol7 <justinb.developer@gmail.com>
2025-01-21 13:19:32 -0400

    finish generics

---
+++ generic-data-types/src/main.rs
@@ -0,0 +1,29 @@
+// We read this definition as: the function largest is generic over some type T. This function has one parameter named list, which is a slice of values of type T. The largest function will return a reference to a value of the same type T.
+
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
+
+// run time cost of generics. there is none
+// use of monomorphization
+//process of turning generic into specific code
+// by filling in concrete types when compile
