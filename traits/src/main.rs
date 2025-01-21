cdb2251b76b44bfbaf2ca06b150cc176c2df9b26
turnwol7 <justinb.developer@gmail.com>
2025-01-21 14:04:21 -0400

    finish traits

---
+++ traits/src/main.rs
@@ -0,0 +1,15 @@
+// import summarize from the lib.rs in src/
+use traits::{Summary, Tweet};
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
