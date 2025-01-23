c2b5188855a5f4fd1db58b98de836d63320475cd
turnwol7 <justinb.developer@gmail.com>
2025-01-23 16:15:57 -0400

    interior mutability example with refCell

---
+++ reference-cell/src/main.rs
@@ -0,0 +1,34 @@
+// RefCell for single threaded scenarios
+// Rc<T> enables multiple owners of the same data; Box<T> and RefCell<T> have single owners.
+// Box<T> allows immutable or mutable borrows checked at compile time; Rc<T> allows only immutable borrows checked at compile time; RefCell<T> allows immutable or mutable borrows checked at runtime.
+// Because RefCell<T> allows mutable borrows checked at runtime, you can mutate the value inside the RefCell<T> even when the RefCell<T> is immutable.
+
+// interior mutability Ref Cell
+// code from outside look immutable but maybe you have a internal method that makes it mutable
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+fn main() {
+    // thsi shows how we can take an immutable value, then mutate it by borrowing ownership with interior mutability
+    // RC = shared mutabile, RefCell =  interior mutability, 
+    // used when you need to data across multiple owners
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
+}
