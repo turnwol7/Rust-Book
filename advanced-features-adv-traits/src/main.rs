abb38a2b6f1c34ee1b3b8e001c58e7c417fab5b7
turnwol7 <justinb.developer@gmail.com>
2025-01-26 16:40:47 -0400

    example on advanced traits

---
+++ advanced-features-adv-traits/src/main.rs
@@ -0,0 +1,16 @@
+// Associated types connect a type placeholder with a trait such that the trait method definitions can use these placeholder types in their signatures.
+
+use std::fmt;
+
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+    }
+}
+
+fn main() {
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {w}");
+}
