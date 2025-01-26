1fc72ebf17ce182af97190a43b8479b22e230cce
turnwol7 <justinb.developer@gmail.com>
2025-01-26 17:20:02 -0400

    folder stucture for macro and derived macro

---
+++ advanced-features-macros/hello_macro/hello_macro_derive/src/lib.rs
@@ -0,0 +1,16 @@
+use proc_macro::TokenStream;
+use quote::quote;
+
+
+
+fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
+}
